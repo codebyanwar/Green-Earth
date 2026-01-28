940df1ef15d1916249322200a45a3d7b7edc8cfb
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2026-01-29 03:43:40 +0600

    catagory product design and development

--- index.js
+++ index.js
@@ -4,7 +4,44 @@ const categoryCard=(id)=>{
     fetch(url)
     .then(resp=>resp.json())
     .then(prom=>{
-        console.log(prom.plants);
+        displayCatagoryCard(prom.plants);
+    })
+}
+
+// "id": 4,
+// "image": "https://i.ibb.co.com/1YzsVWjm/Gulmohar-min.jpg",
+// "name": "Gulmohar",
+// "description": "Known as the ‘Flame of the Forest’, this tree bursts into a vibrant display of red flowers every summer. Perfect for beautifying avenues and gardens.",
+// "category": "Flowering Tree",
+// "price": 400
+
+const displayCatagoryCard=(plants)=>{
+    const catagoryProductContainr=document.getElementById('catagory-product-container');
+    catagoryProductContainr.innerHTML= '';
+
+    plants.forEach(plant=>{
+        const products = document.createElement('div');
+
+        products.innerHTML= `
+            <div class="sm:p-2 md:p-3 bg-white sm:rounded-md md:rounded-lg h-full flex flex-col">
+                <div>
+                    <img class="sm:rounded-md md:rounded-lg h-80 w-full" src="${plant.image}" alt="">
+                </div>
+
+                <div>
+                    <h3 class="text-[14px] md:text-[16px] lg:text-[18px] font-semibold text-[#1F2937] mt-2 mb-1">${plant.name}</h3>
+                    <p class="text-[12px] md:text-[14px] text-[#1F2937]">${plant.description}</p>
+
+                    <div class="flex justify-between items-center mt-3 mb-4">
+                        <h3 class="text-[#15803D] text-[12px] sm:text-[14px] px-3 py-1 bg-[#DCFCE7] rounded-3xl">${plant.category}</h3>
+                        <span class="text-[#1F2937] font-bold text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px]">৳${plant.price}</span>
+                    </div>
+
+                    <button class="btn bg-[#15803D] text-white border border-solid border-[#15803D] hover:text-[#15803D] hover:bg-transparent duration-300 ease-in w-full rounded-3xl">Add to Cart</button>
+                </div>
+            </div>
+        `
+        catagoryProductContainr.appendChild(products);
     })
 }
 
